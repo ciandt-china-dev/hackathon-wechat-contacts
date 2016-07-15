b990f3e1b3f3c5514f9a62954b65d26bc99bdac8
yan dong <yan@ciandt.com>
2016-07-15 15:14:15 +0800

    change html.tpl.php

---
+++ sites/all/themes/wechat_contacts/templates/html.tpl.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Wechat contacts</title>
+    <?php print $styles; ?>
+    <?php print $scripts; ?>
+  </head>
+  <body>
+    Theme test.
+  </body>
+</html>
